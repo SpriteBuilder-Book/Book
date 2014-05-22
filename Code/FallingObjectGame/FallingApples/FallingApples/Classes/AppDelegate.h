75c3d934a70005ed001764e9b3751b52edbf8b3e
Ben-G <Benjamin.Encz@gmail.com>
2014-05-22 08:13:02 -0700

    restructures book with new outline

---
+++ Code/FallingObjectGame/FallingApples/FallingApples/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  FallingApples
+//
+//  Created by Benjamin Encz on 02/04/14.
+//  Copyright Benjamin Encz 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
